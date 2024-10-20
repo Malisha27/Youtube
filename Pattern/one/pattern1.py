30ff71eaa7a041ee87b26bf3228d66cdd5555133
Malisha <27malisha@gmail.com>
2024-10-20 19:11:16 +0530

    Add files via upload
    python code

---
+++ Pattern/one/pattern1.py
@@ -0,0 +1,15 @@
+#    * * * *
+#    * * * *
+#    * * * *
+#    * * * *
+
+n = int(input("Enter a number for first pattern: "))
+
+
+for i in range(n):
+    for j in range(n):
+        print("* ", end='')
+    print()
+        
+    
+        
